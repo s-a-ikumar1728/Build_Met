38e0eaa16500f289499120d53e8a787be95cab0d
Sai Kumar <86108651+s-a-ikumar1728@users.noreply.github.com>
2022-12-06 18:15:13 +0530

    Add files via upload

---
+++ build_met/app.js
@@ -0,0 +1,35 @@
+function click1() {
+    let text1=document.querySelector('#content1');
+    let cardBody1=document.querySelector('#card1');
+    text1.innerText="Good things are associated with blue, like clear days, more than singing the blues. Just the word 'blue' in the singular is full of optimism and positive connotation to most people."
+    cardBody1.style.backgroundColor="blue";
+    cardBody1.style.color="white";
+    text1.style.padding="10px"
+    text1.style.color="white";
+    text1.style.textAlign="justify"
+    text1.style.boxShadow="0 0 15px white"
+}
+function click2() {
+    let text2=document.querySelector('#content2');
+    let cardBody2=document.querySelector('#card2');
+    text2.innerText="With all this talk of Going Green, Buying Green, Living Green, and Green being the new whatever, I've come to realize that, although we had no green, my grandmother was actually the 'greenest' person I've ever known."
+    cardBody2.style.backgroundColor="green";
+    cardBody2.style.color="white";
+    text2.style.padding="10px"
+    text2.style.color="white";
+    text2.style.textAlign="justify"
+    text2.style.boxShadow="0 0 15px white"
+
+}
+function click3() {
+    let text3=document.querySelector('#content3');
+    let cardBody2=document.querySelector('#card3');
+    text3.innerText="Red is the great clarifier - bright and revealing. I can't imagine becoming bored with red - it would be like becoming bored with the person you love."
+    cardBody2.style.backgroundColor="red";
+    cardBody2.style.color="white";
+    text3.style.padding="10px"
+    text3.style.color="white";
+    text3.style.textAlign="justify"
+    text3.style.boxShadow="0 0 15px white"
+
+}
