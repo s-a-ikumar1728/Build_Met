be1ab4a25499e4e90896cc4be9aca429487bd919
Sai Kumar <86108651+s-a-ikumar1728@users.noreply.github.com>
2022-12-07 13:52:26 +0530

    Add files via upload

--- build_met/app.js
+++ build_met/app.js
@@ -1,8 +1,10 @@
 function click1() {
     let text1=document.querySelector('#content1');
     let cardBody1=document.querySelector('#card1');
-    text1.innerText="Good things are associated with blue, like clear days, more than singing the blues. Just the word 'blue' in the singular is full of optimism and positive connotation to most people."
-    cardBody1.style.backgroundColor="blue";
+    let player1=document.querySelector('#player1');
+    player1.innerText="About Virat"
+    text1.innerText="Kohli made his Test debut in 2011. He reached the number one spot in the ICC rankings for ODI batsmen for the first time in 2013. He has won Man of the Tournament twice at the ICC World Twenty20 (in 2014 and 2016). He is also the leading run scorer of all time in T20 Internationals as well as T20 World Cups."
+    cardBody1.style.backgroundColor="red";
     cardBody1.style.color="white";
     text1.style.padding="10px"
     text1.style.color="white";
@@ -12,8 +14,10 @@ function click1() {
 function click2() {
     let text2=document.querySelector('#content2');
     let cardBody2=document.querySelector('#card2');
-    text2.innerText="With all this talk of Going Green, Buying Green, Living Green, and Green being the new whatever, I've come to realize that, although we had no green, my grandmother was actually the 'greenest' person I've ever known."
-    cardBody2.style.backgroundColor="green";
+    let player2=document.querySelector('#player2');
+    player2.innerText="About Pollard"
+    text2.innerText="Kieron Adrian Pollard (born 12 May 1987) is a retired trinidadian cricketer. He has captained the West Indies cricket team in limited overs cricket and Trinbago Knight Riders in CPL. He is a right arm medium - fast bowling all-rounder."
+    cardBody2.style.backgroundColor="blue";
     cardBody2.style.color="white";
     text2.style.padding="10px"
     text2.style.color="white";
@@ -24,8 +28,11 @@ function click2() {
 function click3() {
     let text3=document.querySelector('#content3');
     let cardBody2=document.querySelector('#card3');
-    text3.innerText="Red is the great clarifier - bright and revealing. I can't imagine becoming bored with red - it would be like becoming bored with the person you love."
-    cardBody2.style.backgroundColor="red";
+    let player3=document.querySelector('#player3');
+    player3.innerText="About AB Devillers";
+    text3.style.textShadow="0 0 15px black"
+    text3.innerText="De Villiers has scored 22 Test and 25 ODI centuries. He scored his first Test century against England in 2005 and his first ODI century against West Indies in 2007. He also holds the world record for the fastest 100 and 150 in One-Day Internationals."
+    cardBody2.style.backgroundColor="green";
     cardBody2.style.color="white";
     text3.style.padding="10px"
     text3.style.color="white";
